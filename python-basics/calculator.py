1c1170b155af3f069bb38ac1ca3f978060f023d8
Raul Buehring <rbbarfield10@gmail.com>
2026-04-05 14:33:13 -0400

    Week 1: hello world, calculator

---
+++ python-basics/calculator.py
@@ -0,0 +1,8 @@
+print("Simple Calculator")
+
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+
+print("You entered:", a, "and", b)
+
+##print .\calculator.py
